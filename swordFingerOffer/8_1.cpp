37e44f348f832d71d1ef2130293a21f6d2575c5c
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-08-17 16:42:02 +0800

    Create 8_1.cpp

---
+++ swordFingerOffer/8_1.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+
+using namespace std;
+
+struct BinaryTreeNode
+{
+    int             m_nValue;
+    BinaryTreeNode *m_pLeft;
+    BinaryTreeNode *m_pRight;
+    BinaryTreeNode *m_pParent;
+};
+
+BinaryTreeNode* getNext(BinaryTreeNode *pNode)
+{
+    if(pNode == nullptr)
+        return nullptr;
+
+    BinaryTreeNode *pNext = nullptr;
+
+    // 如果其有右子节点, 就一直向下遍历到最左边的子节点
+    if(pNode->m_pRight != nullptr)
+    {
+        BinaryTreeNode *temp = pNode->m_pRight;
+        while(temp->m_pLeft != nullptr)
+            temp = temp->m_pLeft;
+        pNext = temp;
+    }
+    // 如果没有右子节点, 就判断其是其父子节点的左子节点还是右子节点
+    else if(pNode->m_pParent != nullptr)
+    {
+        BinaryTreeNode *pCurrent = pNode;            // 自己本身
+        BinaryTreeNode *pParent = pCurrent->m_pParent;  // 父节点
+
+        // 如果是父节点的右子节点, 一直向上遍历, 知道遍历到节点是其父节点的左子节点才返回
+        while(pParent != nullptr && pCurrent == pParent->m_pRight)
+        {
+            // 指针向上偏移
+            pCurrent = pParent;
+            pParent = pCurrent->m_pParent;
+        }
+
+        // 返回父节点即可
+        pNext = pParent;
+    }
+
+    return pNext;
+}
+
+
+// ==================== 辅助代码用来构建二叉树 ====================
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+    pNode->m_pParent = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+
+        if(pLeft != nullptr)
+            pLeft->m_pParent = pParent;
+        if(pRight != nullptr)
+            pRight->m_pParent = pParent;
+    }
+}
+
+// 销毁树
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+
+void Test(char* testName, BinaryTreeNode* pNode, BinaryTreeNode* expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: ", testName);
+
+    BinaryTreeNode* pNext = getNext(pNode);
+    if(pNext == expected)
+        printf("Passed.\n");
+    else
+        printf("FAILED.\n");
+}
+
+//            8
+//        6      10
+//       5 7    9  11
+void test1()
+{
+    BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNode9 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNode11 = CreateBinaryTreeNode(11);
+
+    ConnectTreeNodes(pNode8, pNode6, pNode10);
+    ConnectTreeNodes(pNode6, pNode5, pNode7);
+    ConnectTreeNodes(pNode10, pNode9, pNode11);
+
+    Test("Test1", pNode8, pNode9);
+    Test("Test2", pNode6, pNode7);
+    Test("Test3", pNode10, pNode11);
+    Test("Test4", pNode5, pNode6);
+    Test("Test5", pNode7, pNode8);
+    Test("Test6", pNode9, pNode10);
+    Test("Test7", pNode11, nullptr);
+
+    DestroyTree(pNode8);
+}
+
+//            5
+//          4
+//        3
+//      2
+void test2()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNode5, pNode4, nullptr);
+    ConnectTreeNodes(pNode4, pNode3, nullptr);
+    ConnectTreeNodes(pNode3, pNode2, nullptr);
+
+    Test("Test8", pNode5, nullptr);
+    Test("Test9", pNode4, pNode5);
+    Test("Test10", pNode3, pNode4);
+    Test("Test11", pNode2, pNode3);
+
+    DestroyTree(pNode5);
+}
+
+//        2
+//         3
+//          4
+//           5
+void test3()
+{
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNode2, nullptr, pNode3);
+    ConnectTreeNodes(pNode3, nullptr, pNode4);
+    ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+    Test("Test12", pNode5, nullptr);
+    Test("Test13", pNode4, pNode5);
+    Test("Test14", pNode3, pNode4);
+    Test("Test15", pNode2, pNode3);
+
+    DestroyTree(pNode2);
+}
+
+int main()
+{
+    test1();
+    test2();
+    test3();
+}
