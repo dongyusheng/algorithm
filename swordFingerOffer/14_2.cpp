fedd7779b0193e5ddfcde438de68e72e1540dcad
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-08-20 12:07:33 +0800

    Create 14_2.cpp

---
+++ swordFingerOffer/14_2.cpp
@@ -0,0 +1,130 @@
+// https://blog.csdn.net/qq_41453285/article/details/108118791
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int maxProductAfterCutting_solution(int length)
+{
+    if(length < 2)
+        return 0;
+    if(length == 2)
+        return 1;
+    if(length == 3)
+        return 2;
+    
+    // 尽可能多地剪去长度为3的绳子段
+    int timesOf3 = length / 3;
+
+    // 当绳子最后剩下的长度为4的时候, 不能再剪去长度为3的绳子段
+    // 此时更好的办法是把绳子剪成长度为2的两段, 因为2*2 > 3*1
+    if(length - timesOf3 * 3 == 1)
+        timesOf3 -= 1;
+    
+    int timesOf2 = (length - timesOf3 * 3) / 2;
+
+    return (int)(pow(3, timesOf3)) * (int)(pow(2, timesOf2));
+}
+
+void test(const char* testName, int length, int expected)
+{
+    int result1 = maxProductAfterCutting_solution(length);
+    if(result1 == expected)
+        std::cout << "Solution1 for " << testName << " passed." << std::endl;
+    else
+        std::cout << "Solution1 for " << testName << " FAILED." << std::endl;
+}
+
+void test1()
+{
+    int length = 1;
+    int expected = 0;
+    test("test1", length, expected);
+}
+
+void test2()
+{
+    int length = 2;
+    int expected = 1;
+    test("test2", length, expected);
+}
+
+void test3()
+{
+    int length = 3;
+    int expected = 2;
+    test("test3", length, expected);
+}
+
+void test4()
+{
+    int length = 4;
+    int expected = 4;
+    test("test4", length, expected);
+}
+
+void test5()
+{
+    int length = 5;
+    int expected = 6;
+    test("test5", length, expected);
+}
+
+void test6()
+{
+    int length = 6;
+    int expected = 9;
+    test("test6", length, expected);
+}
+
+void test7()
+{
+    int length = 7;
+    int expected = 12;
+    test("test7", length, expected);
+}
+
+void test8()
+{
+    int length = 8;
+    int expected = 18;
+    test("test8", length, expected);
+}
+
+void test9()
+{
+    int length = 9;
+    int expected = 27;
+    test("test9", length, expected);
+}
+
+void test10()
+{
+    int length = 10;
+    int expected = 36;
+    test("test10", length, expected);
+}
+
+void test11()
+{
+    int length = 50;
+    int expected = 86093442;
+    test("test11", length, expected);
+}
+
+int main(int agrc, char* argv[])
+{
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+    test8();
+    test9();
+    test10();
+    test11();
+
+    return 0;
+}
