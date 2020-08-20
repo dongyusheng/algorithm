58169db015ee44228dd8efafd14527861782da65
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-08-20 15:25:57 +0800

    Create 16_1.cpp

---
+++ swordFingerOffer/16_1.cpp
@@ -0,0 +1,85 @@
+// https://blog.csdn.net/qq_41453285/article/details/108123621
+#include <iostream>
+
+using namespace std;
+
+bool g_InvalidInput = false;
+bool equal(double num1, double num2);
+double PowerWithUnsignedExponent(double base, unsigned int exponent);
+
+
+double Power(double base, int exponent)
+{
+    // 如果底数为0, 指数为负数, 直接返回0
+    if(equal(base, 0.0) && exponent < 0)
+    {
+        g_InvalidInput = true;
+        return 0.0;
+    }
+
+    unsigned int absExponent = (unsigned int)exponent;
+    
+    // 如果指数为负数, 现将其转换为正数
+    if(exponent < 0)
+        absExponent = (unsigned int)(-exponent);
+    
+    // 算出base的absExponent次方
+    double result = PowerWithUnsignedExponent(base, absExponent);
+
+    // 如果指数为负数, 那么将得到的结果进行倒数
+    if(exponent < 0)
+        result = 1.0 / result;
+    
+    return result;
+}
+
+bool equal(double num1, double num2)
+{
+    if((num1 - num2 > -0.0000001) && (num1 - num2 < 0.0000001))
+        return true;
+    else
+        return false;
+}
+
+double PowerWithUnsignedExponent(double base, unsigned int exponent)
+{
+    int result = 1.0;
+    for(int i = 1; i <= exponent; ++i)
+        result *= base;
+    return result;
+}
+
+void Test(const char* testName, double base, int exponent, double expectedResult, bool expectedFlag)
+{
+    double result = Power(base, exponent);
+    if (equal(result, expectedResult) && g_InvalidInput == expectedFlag)
+        std::cout << testName << " passed" << std::endl;
+    else
+        std::cout << testName << " FAILED" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    // 底数、指数都为正数
+    Test("Test1", 2, 3, 8, false);
+
+    // 底数为负数、指数为正数
+    Test("Test2", -2, 3, -8, false);
+
+    // 指数为负数
+    Test("Test3", 2, -3, 0.125, false);
+
+    // 指数为0
+    Test("Test4", 2, 0, 1, false);
+
+    // 底数、指数都为0
+    Test("Test5", 0, 0, 1, false);
+
+    // 底数为0、指数为正数
+    Test("Test6", 0, 4, 0, false);
+
+    // 底数为0、指数为负数
+    Test("Test7", 0, -4, 0, true);
+
+    return 0;
+}
