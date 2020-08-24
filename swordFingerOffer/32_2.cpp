513903b81b55307587e7bda7ef649a7b0d2d2b66
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-08-24 18:21:51 +0800

    Create 32_2.cpp

---
+++ swordFingerOffer/32_2.cpp
@@ -0,0 +1,282 @@
+// https://blog.csdn.net/qq_41453285/article/details/108201367
+#include <cstdio>
+#include <queue>
+
+using namespace std;
+
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+
+void Print(BinaryTreeNode *pRoot)
+{
+    if(pRoot == nullptr)
+        return;
+    
+    std::queue<BinaryTreeNode*> _queue;
+    _queue.push(pRoot);
+
+    int nextLevel = 0;
+    int toBePrinted = 1;
+
+    // 队列不为空, 一直打印
+    while(!_queue.empty())
+    {
+        // 取出队首元素, 并打印
+        BinaryTreeNode *temp = _queue.front();
+        _queue.pop();
+        printf("%d ", temp->m_nValue);
+
+        // 如果有左右子节点, 将左右子节点加入到队列中
+        if(temp->m_pLeft != nullptr)
+        {
+            _queue.push(temp->m_pLeft);
+            nextLevel++;
+        }
+        if(temp->m_pRight != nullptr)
+        {
+            _queue.push(temp->m_pRight);
+            nextLevel++;
+        }
+
+        // 当前层打印的还没有打印的节点数目--
+        toBePrinted--;
+
+        // 如果当前层打印完了, 开始打印下一层, 并且换行
+        if(toBePrinted == 0)
+        {
+            toBePrinted = nextLevel;
+            nextLevel = 0;
+            printf("\n");
+        }
+    }
+}
+
+
+// ====================链表操作代码====================
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void PrintTreeNode(const BinaryTreeNode* pNode)
+{
+    if(pNode != nullptr)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is nullptr.\n");
+
+        if(pNode->m_pRight != nullptr)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is nullptr.\n");
+    }
+    else
+    {
+        printf("this node is nullptr.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(const BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != nullptr)
+    {
+        if(pRoot->m_pLeft != nullptr)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != nullptr)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+
+
+// ====================测试代码====================
+//            8
+//        6      10
+//       5 7    9  11
+void Test1()
+{
+    BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+    BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+    BinaryTreeNode* pNode9 = CreateBinaryTreeNode(9);
+    BinaryTreeNode* pNode11 = CreateBinaryTreeNode(11);
+
+    ConnectTreeNodes(pNode8, pNode6, pNode10);
+    ConnectTreeNodes(pNode6, pNode5, pNode7);
+    ConnectTreeNodes(pNode10, pNode9, pNode11);
+
+    printf("====Test1 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("8 \n");
+    printf("6 10 \n");
+    printf("5 7 9 11 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode8);
+    printf("\n");
+
+    DestroyTree(pNode8);
+}
+
+//            5
+//          4
+//        3
+//      2
+void Test2()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNode5, pNode4, nullptr);
+    ConnectTreeNodes(pNode4, pNode3, nullptr);
+    ConnectTreeNodes(pNode3, pNode2, nullptr);
+
+    printf("====Test2 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("5 \n");
+    printf("4 \n");
+    printf("3 \n");
+    printf("2 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode5);
+    printf("\n");
+
+    DestroyTree(pNode5);
+}
+
+//        5
+//         4
+//          3
+//           2
+void Test3()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+
+    ConnectTreeNodes(pNode5, nullptr, pNode4);
+    ConnectTreeNodes(pNode4, nullptr, pNode3);
+    ConnectTreeNodes(pNode3, nullptr, pNode2);
+
+    printf("====Test3 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("5 \n");
+    printf("4 \n");
+    printf("3 \n");
+    printf("2 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode5);
+    printf("\n");
+
+    DestroyTree(pNode5);
+}
+
+void Test4()
+{
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    printf("====Test4 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("5 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode5);
+    printf("\n");
+
+    DestroyTree(pNode5);
+}
+
+void Test5()
+{
+    printf("====Test5 Begins: ====\n");
+    printf("Expected Result is:\n");
+
+    printf("Actual Result is: \n");
+    Print(nullptr);
+    printf("\n");
+}
+
+//        100
+//        /
+//       50   
+//         \
+//         150
+void Test6()
+{
+    BinaryTreeNode* pNode100 = CreateBinaryTreeNode(100);
+    BinaryTreeNode* pNode50 = CreateBinaryTreeNode(50);
+    BinaryTreeNode* pNode150 = CreateBinaryTreeNode(150);
+
+    ConnectTreeNodes(pNode100, pNode50, nullptr);
+    ConnectTreeNodes(pNode50, nullptr, pNode150);
+
+    printf("====Test6 Begins: ====\n");
+    printf("Expected Result is:\n");
+    printf("100 \n");
+    printf("50 \n");
+    printf("150 \n\n");
+
+    printf("Actual Result is: \n");
+    Print(pNode100);
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+
+    return 0;
+}
