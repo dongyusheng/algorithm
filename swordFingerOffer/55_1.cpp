0eee04a8222b380e16578c18cc8481a4741b9b83
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-09-20 12:41:40 +0800

    Create 55_1.cpp

---
+++ swordFingerOffer/55_1.cpp
@@ -0,0 +1,210 @@
+// https://blog.csdn.net/qq_41453285/article/details/108691874
+#include <cstdio>
+
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+
+int TreeDepth(BinaryTreeNode* pRoot)
+{
+    if(pRoot == nullptr)
+        return 0;
+    
+    int nLeft = TreeDepth(pRoot->m_pLeft);
+    int nRight = TreeDepth(pRoot->m_pRight);
+
+    return (nLeft > nRight) ? (nLeft + 1) : (nRight + 1);
+}
+
+// ====================树操作代码====================
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void PrintTreeNode(const BinaryTreeNode* pNode)
+{
+    if(pNode != nullptr)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is nullptr.\n");
+
+        if(pNode->m_pRight != nullptr)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is nullptr.\n");
+    }
+    else
+    {
+        printf("this node is nullptr.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(const BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != nullptr)
+    {
+        if(pRoot->m_pLeft != nullptr)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != nullptr)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
+
+// ====================测试代码====================
+void Test(const char* testName, BinaryTreeNode* pRoot, int expected)
+{
+    int result = TreeDepth(pRoot);
+    if(expected == result)
+        printf("%s passed.\n", testName);
+    else
+        printf("%s FAILED.\n", testName);
+}
+
+//            1
+//         /      \
+//        2        3
+//       /\         \
+//      4  5         6
+//        /
+//       7
+void Test1()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+    BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+    BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+
+    ConnectTreeNodes(pNode1, pNode2, pNode3);
+    ConnectTreeNodes(pNode2, pNode4, pNode5);
+    ConnectTreeNodes(pNode3, nullptr, pNode6);
+    ConnectTreeNodes(pNode5, pNode7, nullptr);
+
+    Test("Test1", pNode1, 4);
+
+    DestroyTree(pNode1);
+}
+
+//               1
+//              /
+//             2
+//            /
+//           3
+//          /
+//         4
+//        /
+//       5
+void Test2()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNode1, pNode2, nullptr);
+    ConnectTreeNodes(pNode2, pNode3, nullptr);
+    ConnectTreeNodes(pNode3, pNode4, nullptr);
+    ConnectTreeNodes(pNode4, pNode5, nullptr);
+
+    Test("Test2", pNode1, 5);
+
+    DestroyTree(pNode1);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+void Test3()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+    BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+    BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+    BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+    ConnectTreeNodes(pNode1, nullptr, pNode2);
+    ConnectTreeNodes(pNode2, nullptr, pNode3);
+    ConnectTreeNodes(pNode3, nullptr, pNode4);
+    ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+    Test("Test3", pNode1, 5);
+
+    DestroyTree(pNode1);
+}
+
+// 树中只有1个结点
+void Test4()
+{
+    BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+    Test("Test4", pNode1, 1);
+
+    DestroyTree(pNode1);
+}
+
+// 树中没有结点
+void Test5()
+{
+    Test("Test5", nullptr, 0);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+
+    return 0;
+}
