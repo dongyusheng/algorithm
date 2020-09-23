28e754b654e84acacf7bc29a822dbcfc20e5ee93
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-09-23 15:59:16 +0800

    Create 10_3.cpp

---
+++ swordFingerOffer/10_3.cpp
@@ -0,0 +1,81 @@
+// https://blog.csdn.net/qq_41453285/article/details/108060505
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+struct Matrix2By2
+{
+    Matrix2By2
+    (
+        long long m00 = 0, 
+        long long m01 = 0, 
+        long long m10 = 0, 
+        long long m11 = 0
+    )
+    :m_00(m00), m_01(m01), m_10(m10), m_11(m11) 
+    {
+    }
+
+    long long m_00;
+    long long m_01;
+    long long m_10;
+    long long m_11;
+};
+
+Matrix2By2 MatrixMultiply
+(
+    const Matrix2By2& matrix1, 
+    const Matrix2By2& matrix2
+)
+{
+    return Matrix2By2(
+        matrix1.m_00 * matrix2.m_00 + matrix1.m_01 * matrix2.m_10,
+        matrix1.m_00 * matrix2.m_01 + matrix1.m_01 * matrix2.m_11,
+        matrix1.m_10 * matrix2.m_00 + matrix1.m_11 * matrix2.m_10,
+        matrix1.m_10 * matrix2.m_01 + matrix1.m_11 * matrix2.m_11);
+}
+
+Matrix2By2 MatrixPower(unsigned int n)
+{
+    assert(n > 0);
+
+    Matrix2By2 matrix;
+    if(n == 1)
+    {
+        matrix = Matrix2By2(1, 1, 1, 0);
+    }
+    else if(n % 2 == 0)
+    {
+        matrix = MatrixPower(n / 2);
+        matrix = MatrixMultiply(matrix, matrix);
+    }
+    else if(n % 2 == 1)
+    {
+        matrix = MatrixPower((n - 1) / 2);
+        matrix = MatrixMultiply(matrix, matrix);
+        matrix = MatrixMultiply(matrix, Matrix2By2(1, 1, 1, 0));
+    }
+
+    return matrix;
+}
+
+long long Fibonacci(unsigned int n)
+{
+    int result[2] = {0, 1};
+    if(n < 2)
+        return result[n];
+
+    Matrix2By2 PowerNMinus2 = MatrixPower(n - 1);
+    return PowerNMinus2.m_00;
+}
+
+int main()
+{
+    std::cout << "Fibonacci 0: " << Fibonacci(0) << std::endl;
+    std::cout << "Fibonacci 1: " << Fibonacci(1) << std::endl;
+    std::cout << "Fibonacci 5: " << Fibonacci(5) << std::endl;
+    std::cout << "Fibonacci 10: " << Fibonacci(10) << std::endl;
+
+    return 0;
+}
