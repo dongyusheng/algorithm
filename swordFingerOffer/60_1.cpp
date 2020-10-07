d88e9a2c0b9812cbe1aae48fab46da4fda3d6411
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-10-07 18:02:46 +0800

    Create 60_1.cpp

---
+++ swordFingerOffer/60_1.cpp
@@ -0,0 +1,77 @@
+// https://blog.csdn.net/qq_41453285/article/details/108944237
+#include <cstdio>
+#include <math.h>
+
+int g_maxValue = 6;
+ 
+void Probability(int number, int* pProbabilities);
+void Probability(int original, int current, int sum, int* pProbabilities);
+
+void PrintProbability(int number)
+{
+    if(number < 1)
+        return;
+ 
+    int maxSum = number * g_maxValue;
+    int* pProbabilities = new int[maxSum - number + 1];
+    for(int i = number; i <= maxSum; ++i)
+        pProbabilities[i - number] = 0;
+ 
+    Probability(number, pProbabilities);
+ 
+    int total = pow((double)g_maxValue, number);
+    for(int i = number; i <= maxSum; ++i)
+    {
+        double ratio = (double)pProbabilities[i - number] / total;
+        printf("%d: %e\n", i, ratio);
+    }
+ 
+    delete[] pProbabilities;
+}
+ 
+void Probability(int number, int* pProbabilities)
+{
+    for(int i = 1; i <= g_maxValue; ++i)
+        Probability(number, number, i, pProbabilities);
+}
+ 
+void Probability(int original, int current, int sum, 
+                 int* pProbabilities)
+{
+    if(current == 1)
+    {
+        pProbabilities[sum - original]++;
+    }
+    else
+    {
+        for(int i = 1; i <= g_maxValue; ++i)
+        {
+            Probability(original, current - 1, i + sum, pProbabilities);
+        }
+    }
+} 
+
+// ====================测试代码====================
+void Test(int n)
+{
+    printf("Test for %d begins:\n", n);
+    
+    PrintProbability(n);
+
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test(1);
+    Test(2);
+    Test(3);
+    Test(4);
+    
+    Test(11);
+
+    Test(0);
+
+    return 0;
+}
+
