8d9ee4dcbca792b01befaac5b90f21db5dd39c17
DongShao <52525198+dongyusheng@users.noreply.github.com>
2020-10-07 18:32:06 +0800

    Create 62_1.cpp

---
+++ swordFingerOffer/62_1.cpp
@@ -0,0 +1,94 @@
+// https://blog.csdn.net/qq_41453285/article/details/108953625
+#include <cstdio>
+#include <list>
+
+using namespace std;
+
+int LastRemaining(unsigned int n, unsigned int m)
+{
+    if(n < 1 || m < 1)
+        return -1;
+
+    unsigned int i = 0;
+
+    list<int> numbers;
+    for(i = 0; i < n; ++ i)
+        numbers.push_back(i);
+
+    list<int>::iterator current = numbers.begin();
+    while(numbers.size() > 1)
+    {
+        for(int i = 1; i < m; ++ i)
+        {
+            current ++;
+            if(current == numbers.end())
+                current = numbers.begin();
+        }
+
+        list<int>::iterator next = ++ current;
+        if(next == numbers.end())
+            next = numbers.begin();
+
+        -- current;
+        numbers.erase(current);
+        current = next;
+    }
+
+    return *(current);
+}
+
+// ====================测试代码====================
+void Test(const char* testName, unsigned int n, unsigned int m, int expected)
+{
+    if(testName != nullptr)
+        printf("%s begins: \n", testName);
+
+    if(LastRemaining(n, m) == expected)
+        printf("passed.\n");
+    else
+        printf("failed.\n");
+
+    printf("\n");
+}
+
+void Test1()
+{
+    Test("Test1", 5, 3, 3);
+}
+
+void Test2()
+{
+    Test("Test2", 5, 2, 2);
+}
+
+void Test3()
+{
+    Test("Test3", 6, 7, 4);
+}
+
+void Test4()
+{
+    Test("Test4", 6, 6, 3);
+}
+
+void Test5()
+{
+    Test("Test5", 0, 0, -1);
+}
+
+void Test6()
+{
+    Test("Test6", 4000, 997, 1027);
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+
+    return 0;
+}
